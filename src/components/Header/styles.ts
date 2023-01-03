0ad98933d2523abc377afff1d0e6f4c738257422
davilucena222 <davilucena222@gmail.com>
2023-01-03 09:44:47 -0300

    styling buy button

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,4 +5,9 @@ export const HeaderContainer = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+
+  display: "flex",
+  alignItens: "center",
+  justifyContent: "space-between",
+  
 });
